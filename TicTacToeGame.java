903ca533b5fa558209c2a9325edb7cec4e957737
ktritsh7 <ktriesh7@gmail.com>
2021-05-10 09:28:16 +0530

    Refactor: TicTacToe Game Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,24 +1,16 @@
 public class TicTacToeGame {
-	
-	static String[] board;
-	
-    static void ticTacToeBoard()
-    {
-        
-        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-        
-        System.out.println("|--------------------|");
-        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-        
-        System.out.println("|--------------------|");
-        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-       
-    }
 
 	public static void main(String[] args) {
-		board = new String[9];
-		ticTacToeBoard();
-
+		char[] board = createBoard();
+	}
+	
+	/* UC1 */
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i = 0; i<board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
 	}
 
 }
