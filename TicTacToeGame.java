9f59d6d3224e8fa7266aec601f60b3b9718c3126
ktritsh7 <ktriesh7@gmail.com>
2021-05-10 11:45:19 +0530

    Added: Move to desired Location

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
-import java.util.*;
 
 public class TicTacToeGame {
-	
-	static char[] board;
 
 	public static void main(String[] args) {
 		char[] board = createBoard();
-		Scanner sc = new Scanner(System.in);
+		Scanner userInput = new Scanner(System.in);
 		char userLetter = chooseUserLetter(userInput);
 		char computerLetter = (userLetter == 'X') ? '0': 'X';
 		
-		board = new char[9];
-		showBoard();
-		char[] board = createBoard();
+		showBoard(board);
 	}
 	
 	/* UC1 */
@@ -27,20 +22,31 @@ public class TicTacToeGame {
 	
 	/* UC2 */
 	public static char chooseUserLetter(Scanner userInput) {
-		System.out.println("Choose your Letter: ");
+		System.out.print("Choose your Letter: ");
 		return userInput.next().toUpperCase().charAt(0);
 	}
 	
 	/* UC3 */
-	public static void showBoard() {
+	public static void showBoard(char[] board) {
 		
 		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
 
-        System.out.println("|--------------------|");
+        System.out.println("|-----------|");
         System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
 
-        System.out.println("|--------------------|");
+        System.out.println("|-----------|");
         System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+	}
+	
+	/* UC4 */
+	public static void moveLocation()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("At which position you want to move: ");
+		int position = sc.nextInt();
+		
+		if(board[position]!='X' &&board[position]!='O')
+		
 	}
 
 }
